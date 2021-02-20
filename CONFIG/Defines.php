45ba5e2bf578483c5312cf31521fbe1c664078e2
Diego Moraes <dmoraes@outlook.com>
2021-02-19 21:32:16 -0300

    Add Filer curso

---
+++ CONFIG/Defines.php
@@ -0,0 +1,16 @@
+﻿<?php
+
+/*
+ * Constantes de par�metros para configura��o da conex�o
+ */
+define('HOST', 'localhost');
+define('DBNAME', 'trabalho_php');
+define('CHARSET', 'utf8');
+define('USER', 'root');
+define('PASSWORD', '');
+
+// Setando Erros do php.ini
+ini_set('display_errors',1);
+ini_set('display_startup_erros',1);
+error_reporting(E_ALL);
+ini_set('memory_limit', '256M');
