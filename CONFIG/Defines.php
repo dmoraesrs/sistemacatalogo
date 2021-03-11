1453c5c3772c11ee2eeef5dce41fb72e4899bfa5
dmoraesrs <44145068+dmoraesrs@users.noreply.github.com>
2021-03-11 10:31:25 -0300

    Update Defines.php

--- CONFIG/Defines.php
+++ CONFIG/Defines.php
@@ -10,9 +10,9 @@ define('USER', 'admindb');
 define('PASSWORD', '12qwaszx@');
 
 // Setando Erros do php.ini
-
+/*
 ini_set('display_errors',1);
 ini_set('display_startup_erros',1);
 error_reporting(E_ALL);
 ini_set('memory_limit', '256M');
-
+*/
